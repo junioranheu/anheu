1874e9176307560899216ac673fc140ee85655ea
junioranheu <junioranheu@gmail.com>
2022-05-12 17:49:34 -0300

    urlApi.js

--- front-end/utils/outros/urlApi.js
+++ front-end/utils/outros/urlApi.js
@@ -1,6 +1,6 @@
 // let API_BASE_URL_DEV = 'https://localhost:44387';
 let API_BASE_URL_DEV = 'http://localhost:57726';
-let API_BASE_URL_PROD = 'https://anheu.azurewebsites.net';
+let API_BASE_URL_PROD = 'https://anheuapi.azurewebsites.net';
 
 // Habilitar dev = prod;
 API_BASE_URL_DEV = API_BASE_URL_PROD;

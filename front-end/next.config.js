2aad90fe5e0990e8529d0e2e4e10c54c3a4be6cd
junioranheu <junioranheu@gmail.com>
2022-05-11 22:00:25 -0300

    instalar pacote "next-videos" + ajustar next.config.js

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withVideos = require('next-videos'); // Habilitar import de vídeos; https://www.npmjs.com/package/next-videos
 
-module.exports = nextConfig
+module.exports = withVideos(
+    {
+        swcMinify: true // Habilitar minify em produção: https://nextjs.org/docs/advanced-features/compiler
+    }
+)

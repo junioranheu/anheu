95b51915c3caa15ed13f7ad30a81e3fd0a777295
junioranheu <junioranheu@gmail.com>
2022-05-31 21:59:19 -0300

    iniciar \posts\index.js + ajustes gerais

---
+++ front-end/pages/posts/index.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import paginaCarregada from '../../utils/outros/paginaCarregada';
+
+export default function Index() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        paginaCarregada(true, 200, 500, setIsLoaded);
+    }, []);
+
+    if (!isLoaded) {
+        return false;
+    }
+
+    return (
+        <section className={'flexColumn paddingPadrao margem50'}>
+            <span className='titulo'>Posts</span>
+        </section>
+    )
+}

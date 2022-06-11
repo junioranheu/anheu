2b6ca3b8e2dbf965eea2d8916e1fad87aa70bb11
junioranheu <junioranheu@gmail.com>
2022-06-11 11:04:55 -0300

    continuar desenvolvimento de criar-post e suas dependências

--- front-end/pages/posts/criar-post.js
+++ front-end/pages/posts/criar-post.js
@@ -1,5 +1,8 @@
 import Router from 'next/router';
-import React, { useContext, useEffect, useState } from 'react';
+import NProgress from 'nprogress';
+import { useContext, useEffect, useRef, useState } from 'react';
+import Botao from '../../components/outros/botao.js';
+import Styles from '../../styles/criarPost.module.css';
 import { Auth, UsuarioContext } from '../../utils/context/usuarioContext';
 import paginaCarregada from '../../utils/outros/paginaCarregada';
 
@@ -13,20 +16,62 @@ export default function CriarPost() {
         paginaCarregada(true, 200, 500, setIsLoaded);
     }, []);
 
+    const refUsuario = useRef();
+    const refSenha = useRef();
+    const refBtn = useRef();
+
+    // Ao alterar os valores dos inputs, insira os valores nas variaveis do formData;
+    const [formData, setFormData] = useState(null);
+    function handleChange(e) {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    // Ao clicar no botão para criar novo post;
+    async function handleSubmit(e) {
+        NProgress.start();
+        refBtn.current.disabled = true;
+        e.preventDefault();
+
+        if (!formData || !formData.usuario || !formData.senha) {
+            NProgress.done();
+            Aviso.warn('O nome de usuário e/ou e-mail estão vazios!', 5000);
+            refSenha.current.value = '';
+            refUsuario.current.select();
+            refBtn.current.disabled = false;
+            return false;
+        }
+    };
+
     if (!isLoaded) {
         return false;
     }
- 
+
     if (usuarioTipoId !== 1) {
         Router.push({ pathname: '/404', query: { msg: 'sem-acesso' } });
         return false;
     }
-  
+
     return (
         <section className={'flexColumn paddingPadrao margem50'}>
             <span className='titulo'>Criar novo post</span>
+            <span className='tituloDesc'>xxx</span>
 
-            {/* INPUTS AQUI */} 
+            <div className='margem10'>
+                <input className={`${Styles.input} ${Styles.margemTopP}`} type='text' placeholder='xxx'
+                    name='xxx' onChange={handleChange} ref={refUsuario}
+                />
+
+                <input className={`${Styles.input} ${Styles.margemTopP}`} type='password' placeholder='yyy'
+                    name='yyy' onChange={handleChange} ref={refSenha}
+                />
+
+                <div className={`${Styles.botaoCustom} ${Styles.margemTopP}`} onClick={handleSubmit}>
+                    <Botao texto={'Criar post'} url={''} isNovaAba={false} Svg='' refBtn={refBtn} isEnabled={true} />
+                </div>
+            </div>
 
             {/* Espaço a mais */}
             <div className='espacoBottom'></div>

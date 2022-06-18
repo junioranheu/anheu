1350311ccacd821bf209e8200fbb3b0f093fa13c
junioranheu <junioranheu@gmail.com>
2022-06-17 22:07:05 -0300

    ajustes gerais

--- front-end/utils/data/constUsuariosCursos.js
+++ front-end/utils/data/constUsuariosCursos.js
@@ -1,4 +1,4 @@
-import { API_BASE_URL_DEV, API_BASE_URL_PROD } from '../outros/urlApi';
+import { API_BASE_URL_DEV, API_BASE_URL_PROD } from '../outros/_urlApi';
 
 const ENDPOINTS = {
     GET_TODOS: 'api/UsuariosCursos/todos',

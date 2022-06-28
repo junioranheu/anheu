24b0052a38d8127825eb31055f8e69573e874db2
junioranheu <junioranheu@gmail.com>
2022-06-27 22:23:14 -0300

    corrigir bug de `&quot;` em modalCookieConsent

--- front-end/components/outros/modalCookieConsent.js
+++ front-end/components/outros/modalCookieConsent.js
@@ -15,7 +15,7 @@ export default function ModalCookieConsent() {
             <div style={{ marginBottom: '12px' }}>
                 <span>A sua privacidade é importante para nós. É política do <b className='cor-principal'>Anheu</b> respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar na plataforma.</span>
                 <br />
-                <span>Clicando no botão <i>"Sim, estou de acordo"</i>, você declara que está de acordo com a utilização dos cookies na plataforma.</span>
+                <span>Clicando no botão <i>'Sim, estou de acordo'</i>, você declara que está de acordo com a utilização dos cookies na plataforma.</span>
             </div>
         </CookieConsent>
     )
